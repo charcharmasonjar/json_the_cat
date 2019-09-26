1643faf4117073c050c47cd486f47b7c90d5ff4f
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-26 14:06:02 -0700

    refactor code by moving the main request logic into a function which is then exported

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,17 @@
 const request = require('request');
-const endPoint = "https://api.thecatapi.com/v1/breeds/search?q=";
-const breed = process.argv[2];
-const URL = endPoint.concat(breed);
 
-request(URL, (error, response, body) => {
-  if (error) throw error;
-  const data = JSON.parse(body);
-  if (!data[0]) throw new Error("Breed does not exist");
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  request(URL, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (!data[0]) {
+      return callback("Breed does not exist", null);
+    }
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
